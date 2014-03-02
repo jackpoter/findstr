8fa613d9826e50eff804b3a1049c2205122e299c
jack poter <onyourpv@gmail.com>
2014-03-02 17:47:28 +0800

    Update test_find1.py

--- test_find1.py
+++ test_find1.py
@@ -5,7 +5,7 @@ f.close()
 nn = 0
 m = 0
 list = []
-while(1):
+while True:
     try:
         n = a1.index('\n',m+1)
         list.append(n)
@@ -16,3 +16,11 @@ while(1):
 print 'done'
 print 'nn:', nn
 print 'list:', list
+
+
+'''
+done
+nn: 19
+list: [5, 7, 101, 169, 259, 337, 479, 581, 663, 717, \
+807, 867, 941, 1003, 1093, 1145, 1181, 1213, 1263]
+'''
